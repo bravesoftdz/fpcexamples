71786ca4fe481e98734316ce8ab546eeb47c03ed
Samuel Lampa <samuel.lampa@gmail.com>
2015-10-29 18:17:18 +0100

    Use try .. finally .. end;

--- stringexamples.pas
+++ stringexamples.pas
@@ -2,20 +2,30 @@ program stringexamples;
 {$mode objfpc} // Don't forget
 
 uses
-    classes, strings, strutils;
+    classes, strings, strutils, RegExpr;
 
 var
     SubStrings: TStringList;
     SubString: String;
+    FooPattern: TRegExpr;
 begin
     // Splitting strings
-    SubStrings := TStringList.Create;
-    SubStrings.Delimiter := ':';
-    SubStrings.StrictDelimiter := True;
-    SubStrings.DelimitedText := 'Foo:Bar';
-    for SubString in SubStrings
-    do
-        WriteLn('SubString: ' + SubString);
-    end.
-    SubStrings.Free
+    try
+        SubStrings := TStringList.Create;
+        SubStrings.Delimiter := ':';
+        SubStrings.StrictDelimiter := True;
+        SubStrings.DelimitedText := 'Foo:Bar';
+        for SubString in SubStrings do
+            WriteLn('SubString: ' + SubString);
+    finally
+        SubStrings.Free;
+    end;
+
+    // Regex replacement
+    try
+        FooPattern := TRegExpr.Create();
+        FooPattern.Expression := '';
+    finally
+        FooPattern.Free;
+    end;
 end.
