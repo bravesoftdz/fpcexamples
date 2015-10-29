79f12bcf98d84fc51eb463e874f3be00a6f75c5d
Samuel Lampa <samuel.lampa@gmail.com>
2015-10-29 16:49:46 +0100

    Added string split example

--- stringexamples.pas
+++ stringexamples.pas
@@ -1,6 +1,20 @@
 program stringexamples;
 {$mode objfpc} // Don't forget
 
+uses
+    classes, strings, strutils;
+
+var
+  SubStrings: TStringList;
+  SubString: String;
 begin
-    WriteLn('Hello World!');
+    // Splitting strings
+    SubStrings := TStringList.Create;
+    SubStrings.Delimiter := ':';
+    SubStrings.StrictDelimiter := True;
+    SubStrings.DelimitedText := 'Foo:Bar';
+    for SubString in SubStrings
+    do
+        WriteLn('SubString: ' + SubString);
+    end.
 end.
